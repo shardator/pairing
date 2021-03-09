d390ef81a8b03a7b362c088d4c28fa99fd07d43b
Gergely Nagy <shardator@gmail.com>
2021-03-09 23:30:20 +0100

    Added some new test cases (with huge numbers)

--- test.py
+++ test.py
@@ -2,6 +2,7 @@
 from __future__ import print_function
 from pairing import pair, depair
 import timeit
+import random
 
 
 def test_pair(a, b):
@@ -9,16 +10,21 @@ def test_pair(a, b):
     return pair(a, b)
 
 
-def run_tests():
+def run_tests(num_random_tests = 0):
     test_pair(22, 33)
     test_pair(2**8, 2**8)
     test_pair(2**16, 2**16)
-
-    try:
-        test_pair(2**52, 2**52)
-    except ValueError:
-        pass  # long integers suffer from some imprecision at this size
-
+    test_pair(2**52, 2**52)
+    test_pair(2**52, 1)
+    test_pair(2**52, 2)
+    test_pair(2**52, 55555555)
+    for i in range(0, num_random_tests):
+        test_pair(random.getrandbits(512), random.getrandbits(512))
+        test_pair(random.getrandbits(512), 0)
+        test_pair(0, random.getrandbits(512))
+        test_pair(random.getrandbits(512), 1)
+        test_pair(1, random.getrandbits(512))
+    
     try:
         test_pair(-1, -1)
     except ValueError:
@@ -28,7 +34,7 @@ def run_tests():
 
 
 if __name__ == '__main__':
-    print(run_tests())
+    print(run_tests(2000))
 
     print("Benchmarking...")
     i = 20000
